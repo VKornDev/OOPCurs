00aa541dd047ecb38e1c1198155cc7e3cd958ebc
INTTT <vald9@mail.ru>
2017-04-09 00:37:40 +0300

    test

---
+++ OOPCurs/MainForm.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace OOPCurs
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle31 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle32 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxFixed = new System.Windows.Forms.ComboBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.buttonSaveAs = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.ColumnName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnFixed = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxName.Location = new System.Drawing.Point(135, 15);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(154, 22);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.CustomFormat = "dd.MM.yyyy";
+            this.dateTimePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.dateTimePicker.Location = new System.Drawing.Point(135, 45);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(154, 22);
+            this.dateTimePicker.TabIndex = 2;
+            this.dateTimePicker.Value = new System.DateTime(2017, 4, 8, 0, 0, 0, 0);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(22, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Марка изделия";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(22, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Дата приёма";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(22, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Состояние";
+            // 
+            // comboBoxFixed
+            // 
+            this.comboBoxFixed.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxFixed.FormattingEnabled = true;
+            this.comboBoxFixed.Items.AddRange(new object[] {
+            "True",
+            "False"});
+            this.comboBoxFixed.Location = new System.Drawing.Point(135, 75);
+            this.comboBoxFixed.Name = "comboBoxFixed";
+            this.comboBoxFixed.Size = new System.Drawing.Size(154, 24);
+            this.comboBoxFixed.TabIndex = 7;
+            this.comboBoxFixed.Text = "True";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAdd.Location = new System.Drawing.Point(352, 15);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(150, 26);
+            this.buttonAdd.TabIndex = 8;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDelete.Location = new System.Drawing.Point(352, 47);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(150, 26);
+            this.buttonDelete.TabIndex = 10;
+            this.buttonDelete.Text = "Удалить";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnName,
+            this.ColumnDate,
+            this.ColumnFixed});
+            this.dataGridView.Location = new System.Drawing.Point(12, 105);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersWidth = 15;
+            this.dataGridView.Size = new System.Drawing.Size(490, 238);
+            this.dataGridView.TabIndex = 11;
+            // 
+            // buttonSaveAs
+            // 
+            this.buttonSaveAs.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSaveAs.Location = new System.Drawing.Point(12, 349);
+            this.buttonSaveAs.Name = "buttonSaveAs";
+            this.buttonSaveAs.Size = new System.Drawing.Size(140, 30);
+            this.buttonSaveAs.TabIndex = 12;
+            this.buttonSaveAs.Text = "Сохранить как XML";
+            this.buttonSaveAs.UseVisualStyleBackColor = true;
+            this.buttonSaveAs.Click += new System.EventHandler(this.buttonSaveAs_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonClear.Location = new System.Drawing.Point(197, 349);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(140, 30);
+            this.buttonClear.TabIndex = 13;
+            this.buttonClear.Text = "Очистить таблицу";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonOpen.Location = new System.Drawing.Point(387, 349);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(115, 30);
+            this.buttonOpen.TabIndex = 14;
+            this.buttonOpen.Text = "Загрузить XML";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // ColumnName
+            // 
+            dataGridViewCellStyle31.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ColumnName.DefaultCellStyle = dataGridViewCellStyle31;
+            this.ColumnName.Frozen = true;
+            this.ColumnName.HeaderText = "Марка изделия";
+            this.ColumnName.Name = "ColumnName";
+            this.ColumnName.ReadOnly = true;
+            this.ColumnName.Width = 200;
+            // 
+            // ColumnDate
+            // 
+            dataGridViewCellStyle32.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.ColumnDate.DefaultCellStyle = dataGridViewCellStyle32;
+            this.ColumnDate.HeaderText = "Дата приёма";
+            this.ColumnDate.Name = "ColumnDate";
+            this.ColumnDate.ReadOnly = true;
+            this.ColumnDate.Width = 150;
+            // 
+            // ColumnFixed
+            // 
+            this.ColumnFixed.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ColumnFixed.HeaderText = "Состояние";
+            this.ColumnFixed.Name = "ColumnFixed";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 391);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonSaveAs);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.comboBoxFixed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.textBoxName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ремонт";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button buttonSaveAs;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnFixed;
+        private System.Windows.Forms.ComboBox comboBoxFixed;
+    }
+}
+
---
+++ OOPCurs/MainForm.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.IO;
+
+namespace OOPCurs
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+            // добавление данных на форму
+        {
+            if (textBoxName.Text == "")
+            {
+                MessageBox.Show("Заполните все поля!", "Ошибка");
+            }
+            else
+            {
+                int n = dataGridView.Rows.Add();
+                dataGridView.Rows[n].Cells[0].Value = textBoxName.Text;
+                dataGridView.Rows[n].Cells[1].Value = dateTimePicker.Value.ToShortDateString();
+                dataGridView.Rows[n].Cells[2].Value = comboBoxFixed.Text;
+            }
+        }
+        
+        private void buttonSaveAs_Click(object sender, EventArgs e)
+            // сохранение данных из формы в файл XML
+        {
+            if (dataGridView.Rows.Count > 0)
+            {
+                try
+                {
+                    DataSet ds = new DataSet();
+                    DataTable dt = new DataTable();
+                    dt.TableName = "Equipment";
+                    dt.Columns.Add("Name");
+                    dt.Columns.Add("Date");
+                    dt.Columns.Add("Fixed");
+                    ds.Tables.Add(dt);
+
+                    foreach (DataGridViewRow rows in dataGridView.Rows)
+                    {
+                        DataRow row = ds.Tables["Equipment"].NewRow();
+                        row["Name"] = rows.Cells[0].Value;
+                        row["Date"] = rows.Cells[1].Value;
+                        row["Fixed"] = rows.Cells[2].Value;
+                        ds.Tables["Equipment"].Rows.Add(row);
+                    }
+                    ds.WriteXml("Data.xml");
+                    MessageBox.Show("XML файл успешно сохранен!", "Выполнено");
+                }
+                catch
+                {
+                    MessageBox.Show("Невозможно сохранить XML файл!", "Ошибка");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Таблица пустая!", "Ошибка");
+            }
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e) 
+            // загрузка данных из файла XML на форму
+        {
+            if (dataGridView.Rows.Count > 0)
+            {
+                MessageBox.Show("Очистите поле перед загрузкой нового файла!", "Ошибка");
+            }
+            else
+            {
+                if (File.Exists("Data.xml"))
+                {
+                    DataSet ds = new DataSet();
+                    ds.ReadXml("Data.xml");
+                    
+                    foreach (DataRow item in ds.Tables["Equipment"].Rows)
+                    {
+                        int n = dataGridView.Rows.Add();
+                        dataGridView.Rows[n].Cells[0].Value = item["Name"];
+                        dataGridView.Rows[n].Cells[1].Value = item["Date"];
+                        dataGridView.Rows[n].Cells[2].Value = item["Fixed"];
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("XML файл не найден!", "Ошибка");
+                }
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e) 
+            // очистить таблицу
+        {
+            if (dataGridView.Rows.Count > 0)
+            {
+                dataGridView.Rows.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Таблица уже пустая!", "Ошибка");
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e) 
+            // удалить выбранную строку
+        {
+            if (dataGridView.SelectedRows.Count > 0)
+            {
+                dataGridView.Rows.RemoveAt(dataGridView.SelectedRows[0].Index);
+            }
+            else
+            {
+                MessageBox.Show("Выберите строку для удаления!", "Ошибка");
+            }
+        }
+    }
+}
